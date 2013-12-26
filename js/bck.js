ac5377a34f58028f995e1661dc11cb6609bdede1
karlos g liberal <karlosgliberal@gmail.com>
2013-12-26 17:54:20 +0100

    cambios de hace tiempo

--- js/bck.js
+++ js/bck.js
@@ -1,32 +1,37 @@
-$(document).ready(function(){
+ $(document).ready(function(){
     var offset2 = $(document).height();
-    var lineHF = offset2 - $("#bottommark").position().top;
+    //tambaño en pixels entre top y la imagen
+    var lineHF = offset2 - $("#bottommark").position().top; 
+
     $(window).scroll(function(){
       var offset1 = $(document).height();
       var offset = $(window).scrollTop();
+      var slide4 = $("#slide4").position().top;
+      var slide5 = $("#slide5").position().top -600;
+      var pixelsSlide4 = slide5 - slide4;
+      var divisionSlide4 = pixelsSlide4/4;
+      console.log(slide4, slide5, divisionSlide4*2, offset);
+
       var lineH = offset1 - $("#bottommark").position().top - offset;
-      var lineHpart = lineHF/5;
+      var lineHpart = lineHF/3;
         $("span").html(lineH);
-            if (lineH > lineHpart*4) {
-                  console.log('1');
+            if (offset < divisionSlide4+slide4) {
                 $("#animation").attr("src", "images/vap1.png");
             }
-            if ((lineH < lineHpart*4) && (lineH > lineHpart*3)) {
-                  console.log('2');
+            if (offset < (divisionSlide4*2)+slide4 && offset > slide4) {
+                  console.log('hola');
                 $("#animation").attr("src", "images/vap2.png");
             }
-            if ((lineH < lineHpart*3) && (lineH > lineHpart*2)) {
-                    console.log('3');
+            else {
+                  console.log('hola');
                 $("#animation").attr("src", "images/vap3.png");
             }
-            if (lineH < lineHpart*2 && lineH > lineHpart*1) {
-                    console.log('4');
-                $("#animation").attr("src", "images/vap2.png");
-            }
-            if (lineH < lineHpart) {
-                  console.log('5');
-                $("#animation").attr("src", "images/vap1.png");
-            }
+            // if ((offset <(divisionSlide4*2)+slide4) && (offset > divisionSlide4+slide4)) {
+            //     $("#animation").attr("src", "images/vap2.png");
+            // }
+            // if (offset <(divisionSlide4*3)+slide4) {
+            //     $("#animation").attr("src", "images/vap3.png");
+            // }
         });
     });
 
